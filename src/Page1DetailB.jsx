cd70b565a7c71785d6e2da956fa744781cc908d0
ka52k <76809740+ka52k@users.noreply.github.com>
2022-06-25 18:09:19 +0900

    section5-23迄

---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,17 @@
+import { useLocation, useHistory } from "react-router-dom";
+
+export const Page1DetailB = () => {
+  const { state } = useLocation();
+  console.log(state);
+
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();
+
+  return (
+    <div>
+      <h1>Page1DetailBページです</h1>
+      <button onClick={onClickBack}>戻る</button>
+    </div>
+  );
+};
